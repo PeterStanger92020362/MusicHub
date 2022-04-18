5e4e6120927106d07fd072671b2cdcdeef8234c0
PeterStanger92020362 <65217016+PeterStanger92020362@users.noreply.github.com>
2022-04-18 14:27:24 +1200

    attempting to update to ReactDom v6

--- client/src/App.js
+++ client/src/App.js
@@ -16,6 +16,8 @@ function App() {
   const [profile, setProfile] = useState(null);
 
   useEffect(() => {
+    document.title = "MusicHub"
+
     setToken(accessToken);
 
     const fetchData = async () => {
@@ -38,33 +40,47 @@ function App() {
           <Router>
             <ScrollToTop />
             <Routes>
-              <Route path="/top-artists">
-                <h1>Top Artists</h1>
-              </Route>
-              <Route path="/top-tracks">
-                <h1>Top Tracks</h1>
-              </Route>
-              <Route path="/playlists/:id">
-                <h1>Playlist</h1>
-              </Route>
-              <Route path="/playlists">
-                <h1>Playlists</h1>
-              </Route>
-              <Route exact path="/">
-                <>
-                  <button onClick={logout}>Log Out</button>
+              <Route path="/top-artists"
+                element={
+                  <h1>Top Artists</h1>
+                }
+              />
+              <Route path="/top-tracks"
+                element={
+                  <h1>Top Tracks</h1>
+                }
+              />
+              <Route 
+                path="/playlists/:id"
+                element = {
+                  <h1>Playlist</h1>
+                }
+              />
+              <Route 
+                path="/playlists"
+                element={
+                  <h1>Playlists</h1>
+                }
+              />
+              <Route 
+                path="/"
+                element={
+                  <>
+                    <button onClick={logout}>Log Out</button>
 
-                  {profile && (
-                    <div>
-                      <h1>{profile.display_name}</h1>
-                      <p>{profile.followers.total} Followers</p>
-                      {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar"/>
-                      )}
-                    </div>
-                  )}
-                </>
-              </Route>
+                    {profile}
+                    {profile && (
+                      <div>
+                        <h1>{profile.display_name}</h1>
+                        <p>{profile.followers.total} Followers</p>
+                        {profile.images.length && profile.images[0].url && (
+                          <img src={profile.images[0].url} alt="Avatar" />
+                        )}
+                      </div>
+                    )}
+                  </>
+                }
+              />
             </Routes>
           </Router>
         )}
