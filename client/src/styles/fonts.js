e54b51f90a3a2d03852983e7d43ce3cfdd17994c
PeterStanger92020362 <65217016+PeterStanger92020362@users.noreply.github.com>
2022-04-19 18:29:05 +1200

    added styling and updated routes

---
+++ client/src/styles/fonts.js
@@ -0,0 +1,23 @@
+import { css } from 'styled-components/macro';
+
+const fonts = css`
+  @font-face {
+
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+
+    font-weight: 900;
+    font-style: normal;
+  }
+`;
+
+export default fonts;
---
+++ client/src/styles/index.js
@@ -0,0 +1,3 @@
+export { default as GlobalStyle } from './GlobalStyle';
+export { default as Fonts } from './fonts';
+export { default as Variables } from './variables';
